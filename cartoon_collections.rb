a3683a6baa0432a128df9b9383202f7007ec106d
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-18 21:18:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,13 @@
 def roll_call_dwarves(array)
   array.each_with_index do |dwarf, index|
-    p "#{index} #{dwarf}"
+    p "#{index + 1} #{dwarf}"
   end
 end
 
 def summon_captain_planet(array)
   calls = array.collect do |call|
-    call.capitalize
-    call + "!"
+    capital_call = call.capitalize
+    capital_call += "!"
   end
 end
 
