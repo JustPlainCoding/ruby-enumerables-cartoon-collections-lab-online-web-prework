60b173cf8fdb69dfa918c5707a28c5bb5e0c4dc6
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-18 21:22:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese# code an argument here(element)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.include? {element}
 end
